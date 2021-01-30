654c3095ab1a4d571ff658dfbdb95a4209a8e947
Adrian <adriangsanchez98@gmail.com>
2021-01-30 22:06:29 +0000

    Add styles to SuccessStoriesComponent.vue

--- src/components/home/SuccessStoriesComponent.vue
+++ src/components/home/SuccessStoriesComponent.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="success-stories" class="success-stories">
     <div class="overlay overlay-black"></div>
+    <div class="wrapper-reflejos">
+      <div class="reflejos"></div>
+      <div class="reflejos segundo"></div>
+    </div>
     <div class="content mx-auto">
       <div class="success-stories-title text-center">
         <h6 class="title font-weight-bold">Success Stories</h6>
@@ -10,12 +14,12 @@
       </div>
       <div class="row justify-content-center align-items-center">
         <div
-          class="col-12 row align-items-center justify-content-center justify-content-md-between"
+          class="col-12 row align-items-center justify-content-center "
           v-for="(review, idx) in reviews"
           :key="idx"
         >
           <div
-            class="col-12 col-md-7 row justify-content-center align-items-center"
+            class="col-12 col-md-7 col-xl-5 row justify-content-center align-items-center"
           >
             <div class="col-6 p-1">
               <SuccessStoriesChangeComponent
@@ -36,7 +40,11 @@
               ></SuccessStoriesChangeComponent>
             </div>
           </div>
-          <div class="col-12 col-md-5 col-xl-4 mt-2 mt-md-0" data-aos="fade-up-left" data-aos-duration="1500">
+          <div
+            class="col-12 col-md-5 col-xl-4 mt-2 mt-md-0"
+            data-aos="fade-up-left"
+            data-aos-duration="1500"
+          >
             <p class="change-content text-white">
               {{ review.text }}
             </p>
@@ -79,12 +87,64 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.wrapper-reflejos {
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  z-index: 5;
+
+  .reflejos {
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+    width: 300px;
+    height: 100%;
+    z-index: 0;
+    background: rgb(255, 255, 255);
+    background: linear-gradient(
+      20deg,
+      rgba(215, 100, 135, 1) 0%,
+      rgba(215, 100, 135, 0) 70%
+    );
+    opacity: 0.2;
+    animation: reflejo 45s infinite;
+  }
+
+  .segundo {
+    animation-direction: reverse;
+  }
+}
+
+@keyframes reflejo {
+  0% {
+    transform: translateX(40vw) skewX(20deg);
+  }
+  20% {
+    transform: translateX(80vw) skewX(20deg);
+  }
+  50% {
+    transform: translateX(40vw) skewX(20deg);
+  }
+  70% {
+    transform: translateX(30vw) skewX(20deg);
+  }
+  85% {
+    transform: translateX(70vw) skewX(20deg);
+  }
+  100% {
+    transform: translateX(40vw) skewX(20deg);
+  }
+}
+
 .success-stories {
   background-image: url("../../assets/home/saragusfit-success-stories.jpg");
   width: 100vw;
   background-size: cover;
 
-  @media screen and (max-width: 599px){
+  @media screen and (max-width: 599px) {
     height: 35rem;
   }
 
