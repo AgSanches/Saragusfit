e58759af7e031b7987069a82f5231523b6c7cb1e
Adrian <adriangsanchez98@gmail.com>
2021-01-30 21:45:20 +0000

    Scrollbar change color.

--- src/App.vue
+++ src/App.vue
@@ -58,6 +58,26 @@ html {
   height: 6.5rem;
 }
 
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: $primary;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
 @import "styles/fonts";
 @import "~bootstrap/scss/bootstrap";
 </style>
