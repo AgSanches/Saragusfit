8a6f573ad15067741f757eeeb43ec0fb0124969b
Adrian <adriangsanchez98@gmail.com>
2021-02-20 19:31:32 +0000

    SuccessStories refactor.

--- src/components/home/SuccessStoriesComponent.vue
+++ src/components/home/SuccessStoriesComponent.vue
@@ -36,34 +36,9 @@
             :key="idx"
             class="success-stories-swiper"
           >
-            <div
-              class="col-12 row align-items-center justify-content-center mx-auto"
-            >
-              <SuccessStoriesImageBasicComponent
-                v-if="review.imageBasic"
-                :image="imageBasic"
-              ></SuccessStoriesImageBasicComponent>
-
-              <SuccessStoriesImageBeforeAndAfterComponent
-                v-else
-                :images="images"
-              ></SuccessStoriesImageBeforeAndAfterComponent>
-
-              <div
-                class="col-12 col-md-5 col-xl-4 mt-2 mt-md-0"
-                data-aos="fade-up-left"
-                data-aos-duration="1500"
-              >
-                <p class="change-content text-white">
-                  {{ review.text }}
-                </p>
-                <p
-                  class="change-user text-center font-italic text-primary font-weight-bold"
-                >
-                  {{ review.user }}
-                </p>
-              </div>
-            </div>
+            <SuccessStoriesWrapperComponent
+              :review="review"
+            ></SuccessStoriesWrapperComponent>
           </swiper-slide>
         </swiper>
       </div>
@@ -72,8 +47,7 @@
 </template>
 
 <script>
-import SuccessStoriesImageBeforeAndAfterComponent from "./partials/SuccessStoriesImageBeforeAndAfterComponent";
-import SuccessStoriesImageBasicComponent from "./partials/SuccessStoriesImageBasicComponent";
+import SuccessStoriesWrapperComponent from "./partials/SuccessStoriesWrapperComponent";
 
 import SwiperCore, {
   EffectCoverflow,
@@ -106,20 +80,14 @@ SwiperCore.use([
 export default {
   name: "SuccessStoriesComponent",
   components: {
-    SuccessStoriesImageBeforeAndAfterComponent,
-    SuccessStoriesImageBasicComponent,
     Swiper,
-    SwiperSlide
+    SwiperSlide,
+    SuccessStoriesWrapperComponent
   },
   data: () => {
     return {
       reviews: content,
       windowWidth: window.innerWidth,
-      images: [
-        require("../../assets/home/saragusfit-photo-sara.jpeg"),
-        require("../../assets/home/saragusfit-photo-agustina.jpeg")
-      ],
-      imageBasic: require("../../assets/home/saragusfit-feedback.jpg"),
       doc: "successStories"
     };
   },
---
+++ src/components/home/partials/SuccessStoriesWrapperComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="col-12 row align-items-center justify-content-center mx-auto">
+    <SuccessStoriesImageBasicComponent
+      v-if="review.imageBasic"
+      :image="imageBasic"
+    ></SuccessStoriesImageBasicComponent>
+
+    <SuccessStoriesImageBeforeAndAfterComponent
+      v-else
+      :images="images"
+    ></SuccessStoriesImageBeforeAndAfterComponent>
+
+    <div
+      class="col-12 col-md-5 col-xl-4 mt-2 mt-md-0"
+      data-aos="fade-up-left"
+      data-aos-duration="1500"
+    >
+      <p class="change-content text-white">
+        {{ review.text }}
+      </p>
+      <p
+        class="change-user text-center font-italic text-primary font-weight-bold"
+      >
+        {{ review.user }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import SuccessStoriesImageBeforeAndAfterComponent from "./SuccessStoriesImageBeforeAndAfterComponent";
+import SuccessStoriesImageBasicComponent from "./SuccessStoriesImageBasicComponent";
+
+export default {
+  name: "SuccessStoriesWrapper",
+  props: {
+    review: Object
+  },
+  data: () => {
+    return {
+      images: [
+        require("../../../assets/home/saragusfit-photo-sara.jpeg"),
+        require("../../../assets/home/saragusfit-photo-agustina.jpeg")
+      ],
+      imageBasic: require("../../../assets/home/saragusfit-feedback.jpg")
+    };
+  },
+  components: {
+    SuccessStoriesImageBeforeAndAfterComponent,
+    SuccessStoriesImageBasicComponent
+  }
+};
+</script>
+
+<style scoped></style>
